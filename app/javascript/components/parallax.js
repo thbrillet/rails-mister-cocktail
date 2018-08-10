9c44fbf78e95071c22c4062bcf3cbca7489ef840
Thibault Brillet <thbrillet@gmail.com>
2018-08-10 15:01:37 +0200

    Add dotenv - Protect my secret data in .env file

--- app/javascript/components/parallax.js
+++ app/javascript/components/parallax.js
@@ -2,13 +2,13 @@ function scrollFunc() {
   const windowScroll = this.scrollY;
 
   const $logo = document.getElementsByClassName('main-el')[0];
-  $logo.style.transform = 'translateY(' + windowScroll/2 + '%)';
+  $logo.style.transform = 'translateY(-' + windowScroll/4 + '%)';
 
   const $backBird = document.getElementsByClassName('back-el')[0];
   $backBird.style.transform = 'translateY(' + windowScroll/4 + '%)';
 
   const $foreBird = document.getElementsByClassName('fore-el')[0];
-  $foreBird.style.transform = 'translateY(-' + windowScroll/100 + '%)';
+  $foreBird.style.transform = 'translateY(-' + windowScroll/2 + '%)';
 }
 
 window.addEventListener('scroll', scrollFunc);
